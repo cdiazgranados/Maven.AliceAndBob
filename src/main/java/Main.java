5859033f2b8eda5367f373a5197e571ca34f1d8d
Carolina <carolina@Zipcoders-MacBook-Pro.local>
2022-10-27 15:11:22 -0400

    README requirements complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args){
 
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = myObj.next();
+
+        System.out.print("Hi " + name + "! ");
+
+        if (name.equals("Bob") || name.equals("Alice")) {
+            System.out.println("There you are!");
+        } else {
+            System.out.println("I was looking for Alice or Bob...");
+        }
     }
 }
